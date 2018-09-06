5b9c57a300e538bff8a8914054296c53d56f03bb
Erik Engheim <erik.engheim@mac.com>
2018-09-06 23:35:13 +0200

    Refactoring: Put stuff in their right place

--- src/primitives.jl
+++ src/primitives.jl
@@ -19,7 +19,7 @@ end
 
 ##################### IO #####################
 
-function show(io::IO, orientation::Orientation, depth::Integer = 0)
+function show(io::IO, orientation::Orientation)
     if VERTICAL == orientation
         print(io, "VERTICAL")
     elseif HORIZONTAL == orientation
@@ -31,3 +31,35 @@ function show(io::IO, orientation::Orientation, depth::Integer = 0)
 end
 
 ##################### XML #####################
+
+function xml(r::Rect)
+    parent = ElementNode("rect")
+
+    addchildren!(parent, [
+        "x" => string(r.x),
+        "y" => string(r.y),
+        "width" => string(r.width),
+        "height" => string(r.height)])
+    parent
+end
+
+function xml(sz::Size)
+    parent = ElementNode("size")
+
+    addchildren!(parent, [
+        "width"  => string(sz.width),
+        "height" => string(sz.height)])
+    parent
+end
+
+function xml(orientation::Orientation)
+    s = if orientation == HORIZONTAL
+            "Qt::Horizontal"
+        elseif orientation == VERTICAL
+            "Qt::Vertical"
+        end
+    ElementNode("enum", s)
+end
+
+xml(on::Bool) = ElementNode("bool", string(on))
+xml(text::AbstractString) = ElementNode("string", text)
