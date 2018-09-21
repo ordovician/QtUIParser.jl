8b41d68c632c45d1f2612fce2af76b85e84df4ea
Erik Engheim <erik.engheim@mac.com>
2018-09-22 01:57:22 +0200

    Partially done depth convert

--- examples/depthconvert.jl
+++ examples/depthconvert.jl
@@ -1,50 +1,61 @@
-using QtUIParser
-
-ui = Ui(QWidget(
-    class  = "QWidget",
-    name   = "DepthConvertPanel",
-    geometry = Rect(0, 0, 505, 615),
-    windowTitle = "Depth Convert Seismic",
-    layout = VBoxLayout(
-        name = "top",
-        items = [
-            GroupBox(
-                name = "general",
-                layout = HBoxLayout(
-                    name = "general_layout",
+Ui(
+    class   = "DepthConvertPanel",
+    version = "4.0",
+    root = QWidget(
+        name        = "DepthConvertPanel",
+        class       = :QWidget,
+        geometry    = Rect(0, 0, 505, 263),
+        windowTitle = "Depth Convert Seismic",
+        layout = VBoxLayout(
+            name = "top",
+            items = [
+                QGroupBox(
+                    name = "general",
+                    layout = HBoxLayout(
+                        name = "general_layout",
+                        items = [
+                            GridLayout(
+                                name = "output_layout",
+                                items = [
+                                    GridItem(0, 0,
+                                        QLabel("name_label", "Output name:")),
+                                    GridItem(0, 1,
+                                        QLineEdit("name_edit", "Output name:"))
+                                ]
+                            ),
+                            VBoxLayout(
+                                name = "type_layout",
+                                items = [
+                                    QRadioButton("realized", "Realized"),
+                                    QRadioButton("preview", "Preview"),
+                                    Spacer("type_spacer", VERTICAL, Size(10, 20))
+                                ]
+                            )
+                        ]
+                    )
+                ),
+                GridLayout(
+                    name = "cropping_layout",
                     items = [
-                        GridLayout(
-                            name = "output_layout",
-                            items = [
-                                GridItem(0, 0, Label("name_label", "Output name:")),
-                                GridItem(0, 1, LineEdit("name_edit", "Output name:"))
-                            ]
-                        ),
-                        
-                        VBoxLayout(
-                            name = "type_layout",
-                            items = [
-                                RadioButton("realized", "Realized"),
-                                RadioButton("preview", "Preview"),
-                                Spacer("type_spacer", VERTICAL, Size(10, 20))
-                            ]
-                        )
+                        GridItem(0, 0,
+                            QCheckBox("depth_crop", "Cropping in depth")),
+                        GridItem(2, 0,
+                            QLabel("range_label", "Sample range:")),
+                        GridItem(1, 1,
+                            QLabel("min_label", "Min")),
+                        GridItem(2, 1,
+                            QLineEdit("min_edit", "0.0")),
+                        GridItem(3, 1,
+                            QSlider("min_slider", HORIZONTAL)),
+                        GridItem(1, 2,
+                            QLabel("max_label", "Max")),
+                        GridItem(2, 2,
+                            QLineEdit("max_edit", "0.0")),
+                        GridItem(3, 2,
+                            QSlider("max_slider", HORIZONTAL))
                     ]
                 )
-            ),
-            GridLayout(
-                name = "cropping_layout",
-                items = [
-                    GridItem(0, 0, CheckBox("depth_crop", "Cropping in depth")),
-                    GridItem(2, 0, Label(   "range_label","Sample range:")),
-                    GridItem(1, 1, Label(   "min_label", "Min")),                    
-                    GridItem(2, 1, LineEdit("min_edit",  "0.0")),
-                    GridItem(3, 1, Slider("min_slider")),
-                    GridItem(1, 2, Label(   "max_label", "Max")),
-                    GridItem(2, 2, LineEdit("max_edit",  "0.0")),
-                    GridItem(3, 2, Slider("max_slider"))
-                ]
-            )
-        ]
-    )    
-))
+            ]
+        )
+    )
+)
