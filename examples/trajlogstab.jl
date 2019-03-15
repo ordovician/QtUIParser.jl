73cd863704f0cc6ef97b97e0d69327925e3cc68a
Erik Engheim <Erik.Engheim@Emerson.com>
2019-03-15 18:14:58 +0100

    Experiments with more advance UI for traj
    This is based on some real world UI work. Added here to having some more real world
    examples.

--- examples/trajlogstab.jl
+++ examples/trajlogstab.jl
@@ -4,7 +4,7 @@ Ui(
     root = QWidget(
         name     = "traj_logs_tab_",
         class    = :QWidget,
-        geometry = Rect(0, 0, 587, 478),
+        geometry = Rect(0, 0, 652, 492),
         layout = GridLayout(
             name = "trajlogs_gridlayout",
             items = [
@@ -12,31 +12,44 @@ Ui(
                     QLabel("logrun_label_", "logrun:")),
                 GridItem(6, 2,
                     QRadioButton("uncertainty_log_radio_", "Uncertainity log:")),
-                GridItem(0, 1, 4,
+                GridItem(0, 1, 1, 4,
                     QComboBox("logrun_combo_")),
-                GridItem(1, 3, 2,
+                GridItem(1, 3, 1, 2,
                     QComboBox("zonelog_combo_")),
+                GridItem(1, 6,
+                    QPushButton("map_zone_button_", "map zone codes...")),
+                GridItem(2, 6,
+                    QLabel(
+                        name       = "map_zone_label_",
+                        text       = "For discrete logs you need to specify how zones names shoud be mapped to zone codes",
+                        font       = Font(9),
+                        textFormat = PLAIN_TEXT,
+                        wordWrap   = true,
+                        indent     = 10
+                    )),
                 GridItem(2, 2,
                     QLabel(
                         name      = "wells_with_log_label_",
                         text      = "wells with log:",
                         alignment = Alignment[RIGHT, TRAILING, VCENTER]
                     )),
-                GridItem(1, 1, 2,
+                GridItem(3, 2,
+                    Spacer("zonelog_spacer", VERTICAL, Size(20, 40))),
+                GridItem(1, 1, 1, 2,
                     QLabel(
                         name      = "zonelog_label_",
                         text      = "condition zones on zonelog:",
                         alignment = Alignment[LEADING, LEFT, VCENTER]
                     )),
-                GridItem(4, 1, 3,
+                GridItem(4, 1, 1, 3,
                     QCheckBox("vertical_shift_checkbox_", "Allow vertical shifts")),
-                GridItem(2, 3,
+                GridItem(2, 3, 2, 2,
                     QWidget("wells_with_log_list_", :QListWidget)),
-                GridItem(6, 3,
+                GridItem(6, 3, 1, 2,
                     QComboBox("uncertainty_log_combo_")),
-                GridItem(5, 2,
+                GridItem(5, 2, 1, 2,
                     QRadioButton("sd_range_uncertainty_radio_", "Standard deviation range of uncertainty:")),
-                GridItem(5, 3,
+                GridItem(5, 4,
                     QWidget(
                         name          = "sd_range_uncertainty_spinner_",
                         class         = :QDoubleSpinBox,
@@ -45,14 +58,16 @@ Ui(
                         maximum       = 1000.0,
                         value         = 100.0
                     )),
-                GridItem(7, 3,
+                GridItem(7, 3, 2, 2,
                     QWidget("wells_with_uncert_log_list_", :QListWidget)),
                 GridItem(7, 2,
                     QLabel(
                         name      = "wells_with_uncert_log_label_",
                         text      = "wells with log:",
                         alignment = Alignment[RIGHT, TRAILING, VCENTER]
-                    ))
+                    )),
+                GridItem(8, 2,
+                    Spacer("uncertlog_spacer", VERTICAL, Size(20, 40)))
             ]
         )
     )
